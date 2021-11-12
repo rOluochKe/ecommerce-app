3332ae8516416f526bdd72af9f201232d20336db
roluochKe <rasholuoch@gmail.com>
2021-11-12 16:47:15 +0300

    firebase deploy issues

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
+  extends: ["eslint:recommended", "google"],
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
+    "max-len": ["error", {code: 200}],
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
   },
 };
--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,38 @@
-const functions = require('firebase-functions')
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const app = express()
+const app = express();
 
 app.use(
-  cors({
-    origin: true,
-  })
-)
-app.use(express.json())
+    cors({
+      origin: true,
+    }),
+);
+app.use(express.json());
 
-app.post('/payments/create', async (req, res) => {
+app.post("/payments/create", async (req, res) => {
   try {
-    const { amount, shipping } = req.body
+    const {amount, shipping} = req.body;
     const paymentIntent = await stripe.paymentIntents.create({
       shipping,
       amount,
-      currency: 'usd',
-    })
+      currency: "usd",
+    });
 
-    res.status(200).send(paymentIntent.client_secret)
+    res.status(200).send(paymentIntent.client_secret);
   } catch (err) {
     res.status(500).json({
       statusCode: 500,
       message: err.message,
-    })
+    });
   }
-})
+});
 
-app.get('*', (req, res) => {
-  res.status(404).send('404, Not Found.')
-})
+app.get("*", (req, res) => {
+  res.status(404).send("404, Not Found.");
+});
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
